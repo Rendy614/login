a77dc0804fad25d08411eb537084c7c097d7fb32
Rendy614 <rendylistanto123@gmail.com>
2022-05-28 14:28:42 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>RHYTHM</title>
+
+    <!-- menyisipkan bootstrap -->
+     <link rel="stylesheet" href="css/bootstrap.min.css" />
+</head>
+<body class="bg-light">
+    <header>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1>MODE LATIHAN</h1>
+                        <p>Selamat Datang di Mode Latihan</p>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="login.php" class="btn btn-secondary">Masuk</a>
+                        <a href="register.php" class="btn btn-success">Daftar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-100">
+                    <img class="img img-responsive" src="img/connect.jpg" style="width:400px;height:200px" />
+                </div>
+            </div>
+        </div>
+    </section>
+
+</body>
+</html>
